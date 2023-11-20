6275d152e73c1c130bb8418c15e4ae7e5b1f39ce
jz116 <146971345+jz116@users.noreply.github.com>
2023-11-20 11:31:55 -0800

    Update the whole website
    Update the whole website

---
+++ dinner.js
@@ -0,0 +1,11 @@
+/*
+ * dinner.js
+ * Author: Akira Shemansky, Angela Ambriz, Jiecheng Zhu, Kaya Levin, Raquel Herrera-Bernardino, and shiyen yang
+ * Date: Nov. 20, 2023
+ * License: Public Domain
+ */
+
+// Functions
+$("#Options").click(function(){
+
+});
