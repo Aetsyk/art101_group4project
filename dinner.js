ce2d8fe6da0f25d9a6be0f790fb20018c86d9b68
alegnaa <angela.amb040728@gmail.com>
2023-12-11 11:30:15 -0800

    new api key

--- dinner.js
+++ dinner.js
@@ -8,7 +8,7 @@
 // important for the ajax call
 const endPoint = "https://api.spoonacular.com/recipes/complexSearch";
 const apiKey = "da77cacb407a489a896802b680ad1b4f";
-const apiKeyAlt = "daeb818317fb4f3390d97d88c06f592f";
+const apiKeyAlt = "b5c0308679ef49a9875f029b372cce42";
 
 // variables for questionnaire info to be sent to the API
 var intoleranceList = [];
